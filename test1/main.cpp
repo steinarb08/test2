e1e69727047d10a371a326138e176d8a1c19a87d
steinarb08 <steinarbjornsson@hotmail.com>
2015-11-26 11:49:07 +0000

    prufu commit 1

---
+++ test1/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    cout << "hello world!";
+    return 0;
+}
+
